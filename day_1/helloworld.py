94d7d19ca41b7ed785acd5f062836ec2fb0dffcd
Aleksandar <aleksandar.jaglicic@outlook.com>
2025-11-23 15:11:32 +0100

    Add initial implementation of helloworld.py with basic operations and type checks

---
+++ day_1/helloworld.py
@@ -0,0 +1,38 @@
+#%%
+
+print('Aleksandar')
+# %%
+print(3 * 546)
+# %%
+print(3.6755 * 6704.3)
+print(3 ** 13)
+print(4 + 5j)
+# %%
+type(['Aleks', 'Jonathan', 'Marta'])
+# %%
+type(4)
+type(4.5 + 6j)
+# %%
+type('Aleks')
+
+# %%
+aleks_ist_cool = True
+alexander_ist_cool = False
+# %%
+print(aleks_ist_cool)
+# %%
+print(alexander_ist_cool)
+# %%
+print(['Banane', 10, 3.14, False])
+# %%
+# euklidische distanz zwischen zwei punkten (3,4) und (7,1)
+import math
+distanz = math.sqrt((7 - 3) ** 2 + (1 - 4) ** 2)
+print(distanz)
+
+distanz = math.dist((5, 5), (1, 1))
+print(distanz)
+
+distanz = math.sqrt((5 - 1)**2 + (5 - 1)**2)
+print(distanz)
+# %%
---
+++ helloworld.py
@@ -0,0 +1,49 @@
+# %%
+#helloworld.py
+
+
+"""das ist ein multiline comment
+hier kann man über zeilen kommentieren
+das ist auch cool
+"""
+
+ # Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)    #addition (+)
+print(3 - 1)    #subtraction (-)
+print(2 * 3)    #multiplication (*)
+
+# %%
+print(3 + 4)    #addition
+print(3 - 4)    #subtraction
+print(3 * 4)    #multiplication
+print(3 % 4)    #modulus
+print(3 // 4)   #floor division
+
+# %%
+print(type({'name':'Aleksandar'}))
+print(type('Aleksandar'))
+print(type(1992))
+print(type(3.14))
+print(type([1, 2, 3]))
+print(type((1)))
+print(type((9.8, 3.14)))
+print(type({1, 2, 3}))
+
+# %%
+print('Aleksandar')
+print('Jagli\vci\'c')
+print('Österreich')
+print('I am enjoying 30days of python')
+
+# %%
+# check data types in terminal
+
+type('Hello World')      # str
+#%%
+type(10)
+type(9.1745999)
+type(3 + 4j)
+type(['Aleks', 'Boki', 'Ceca'])
+type('Aleksandar')
+# %%
